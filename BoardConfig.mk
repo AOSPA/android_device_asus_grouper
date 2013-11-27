168fb40e56b7f945b0f7f78dec651462b2f41a78
Carlo Savignano <stevewatersy@gmail.com>
2013-11-27 03:23:28 +0100

    PA clean up.
    * Remove aosp stuffs.
    * Fix inheriting props.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,7 +37,6 @@ TARGET_RECOVERY_UI_LIB := librecovery_ui_grouper
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/asus/grouper
 
--include vendor/asus/grouper/BoardConfigVendor.mk
 include device/asus/grouper/BoardConfigCommon.mk
 
 TARGET_RECOVERY_FSTAB = device/asus/grouper/fstab.grouper
